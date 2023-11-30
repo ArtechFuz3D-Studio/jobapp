fff213ec595ac18bb5e245b5c2609ddd44796632
clickonrefresh <72121107+clickonrefresh@users.noreply.github.com>
2023-11-30 15:51:42 +0200

    up

---
+++ app/_layout.js
@@ -0,0 +1,23 @@
+import { Stack } from "expo-router";
+
+const Layout = () => {
+    // const [fontsLoaded] = useFonts({
+    //   DMBold: require("../assets/fonts/DMSans-Bold.ttf"),
+    //   DMMedium: require("../assets/fonts/DMSans-Medium.ttf"),
+    //   DMRegular: require("../assets/fonts/DMSans-Regular.ttf"),
+    // });
+  
+    // if (!fontsLoaded) {
+    //   return null;
+    // }
+  
+    // return (
+    //   <Stack initialRouteName="home">
+    //     <Stack.Screen name="home" />
+    //   </Stack>
+    // )
+
+    return <Stack/>
+  };
+  
+  export default Layout;
