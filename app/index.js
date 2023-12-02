f5dae8ff27d3cbe15fece55eedeadf8f3e909056
StudioHuit <64701-clickonrefresh@users.noreply.drupalcode.org>
2023-12-02 04:11:54 +0200

    sssss

--- app/index.js
+++ app/index.js
@@ -1,10 +1,13 @@
 import { useState } from 'react'
 import {View, ScrollView, Text, } from 'react-native'
+import { Stack, useRouter} from 'expo-router'
+
+import { COLORS, SIZES, icons, images} from '../constants'
 
 const Home = () => {
     return (
         <View>
-            <Text>Home</Text>
+            <Text>Hossssssssssssssssssssssssssssssssme</Text>
         </View>
     )
 }
